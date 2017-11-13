1e6eaaedea00afea8ff502ea440d3faa6f40ed7e
Mulia Hanif <muliahanif10@gmail.com>
2017-11-13 10:11:09 +0700

    Add files via upload

--- InformasiAkademik/src/test/java/com/tubes/informasiakademik/test/DemoIPKContext.java
+++ InformasiAkademik/src/test/java/com/tubes/informasiakademik/test/DemoIPKContext.java
@@ -6,29 +6,16 @@
 package com.tubes.informasiakademik.test;
 
 import cucumber.api.java.bm.Maka;
-import cucumber.api.java.id.Dengan;
 import cucumber.api.java.id.Ketika;
 import junit.framework.Assert;
-
-
 
 /**
  *
  * @author Munip
  */
-public class DemoContext {
-    
-@Dengan("^Nilai mahasiswa sudah dimasukkan ke sistem$")
-public void nilai_mahasiswa_sudah_dimasukkan_ke_sistem() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-//    throw new PendingException();
-    boolean expexted = false;
-    boolean result = false;
-    Assert.assertEquals(expexted, result);
-}
-    
-@Ketika("^Mahasiswa menyelesaikan semester tertentu$")
-public void mahasiswa_menyelesaikan_semester_tertentu() throws Throwable {
+public class DemoIPKContext {
+    @Ketika("^Mahasiswa menyelesaikan tingkat tertentu$")
+public void mahasiswa_menyelesaikan_tingkat_tertentu() throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 //    throw new PendingException();
     boolean expexted = false;
@@ -36,13 +23,12 @@ public void mahasiswa_menyelesaikan_semester_tertentu() throws Throwable {
     Assert.assertEquals(expexted, result);
 }
 
-@Maka("^respon nilai ips terakumulasi$")
-public void respon_nilai_ips_terakumulasi() throws Throwable {
+@Maka("^respon nilai ipk terakumulasi dari nilai ips$")
+public void respon_nilai_ipk_terakumulasi_dari_nilai_ips() throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 //    throw new PendingException();
     boolean expexted = false;
     boolean result = false;
     Assert.assertEquals(expexted, result);
 }
-
 }
